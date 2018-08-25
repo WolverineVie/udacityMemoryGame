ae6bf10a17d94ec0b10447d7dacbf6ebbade6a0e
WolverineVie <38679783+WolverineVie@users.noreply.github.com>
2018-08-25 16:54:05 +0200

    upd

--- js/app.js
+++ js/app.js
@@ -2,8 +2,7 @@
  * Create a list that holds all of your cards
  */
 
-var cardList1 = ["fa-diamond","fa-diamond","fa-paper","fa-paper","fa-anchor","fa-anchor","fa-bolt","fa-bolt",
-                "fa-cube","fa-cube","fa-leaf","fa-leaf","fa-bicycle","fa-bicycle","fa-diamond","fa-diamond","fa-bomb","fa-bomb"];
+var cardList2 = ["fa-diamond","fa-diamond","fa-diamond","fa-diamond","fa-paper","fa-paper","fa-anchor","fa-anchor","fa-bolt","fa-bolt","fa-cube","fa-cube","fa-leaf","fa-leaf","fa-bicycle","fa-bicycle","fa-bomb","fa-bomb"];
 
 
 /*
@@ -67,4 +66,3 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
-
