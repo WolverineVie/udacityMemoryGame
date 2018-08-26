445185deb3846f69b8bf626c83103cd14ac4639d
WolverineVie <38679783+WolverineVie@users.noreply.github.com>
2018-08-26 02:46:46 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -66,3 +66,14 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+var clickedCard = document.getElementsByClassName("fa");
+
+function displayCard(){
+  this.addClassName += " open show";
+};
+
+for( var i=0; i <= clickedCard.lenght; i++){
+  clickedCard[0].addEventListener("click",function(){
+    function displayCard();
+  });
+};
